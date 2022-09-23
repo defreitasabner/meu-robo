21862c44fb9f2efded54f03cbe777b2377d7c0f6
Abner Freitas <defreitasabner@gmail.com>
2022-09-22 22:32:51 -0300

    Leds transferidos para arquivo led.hpp.

---
+++ include/leds.hpp
@@ -0,0 +1,66 @@
+#include <Arduino.h>
+
+#include "pinagem.hpp"
+
+
+namespace leds {
+    
+    void initLeds() {
+        pinMode(pins::PIN_LED_VERDE, OUTPUT);
+        pinMode(pins::PIN_LED_AMARELO, OUTPUT);
+        pinMode(pins::PIN_LED_VERMELHO, OUTPUT);
+    }
+
+    void ligarLedVerde() {
+        digitalWrite(pins::PIN_LED_VERDE, HIGH); 
+    }
+
+    void desligarLedVerde() {
+        digitalWrite(pins::PIN_LED_VERDE, LOW); 
+    }
+
+    void ligarLedAmarelo() {
+        digitalWrite(pins::PIN_LED_AMARELO, HIGH);
+    }
+
+    void desligarLedAmarelo() {
+        digitalWrite(pins::PIN_LED_AMARELO, LOW); 
+    }
+
+
+    void ligarLedVermelho() {
+        digitalWrite(pins::PIN_LED_VERMELHO, HIGH);
+    }
+
+    void desligarLedVermelho() {
+        digitalWrite(pins::PIN_LED_VERMELHO, LOW); 
+    }
+
+    void verificaLeds(long distancia) {
+        
+        if(distancia < 100 && distancia >= 50) {
+            ligarLedVerde();
+            desligarLedAmarelo();
+            desligarLedVermelho();
+        }
+        
+        else if(distancia < 50 && distancia >= 5) {
+            desligarLedVerde();
+            ligarLedAmarelo();
+            desligarLedVermelho();
+        }
+        
+        else if(distancia < 5) {
+            desligarLedVerde();
+            desligarLedAmarelo();
+            ligarLedVermelho();
+        }
+
+        else {
+            desligarLedVerde();
+            desligarLedAmarelo();
+            desligarLedVermelho();
+        }
+    }
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,15 @@
 #include <Arduino.h>
 
 #include "sensorUltrassonico.hpp"
-
-#define PIN_LED_VERDE 4
-#define PIN_LED_AMARELO 7 
-#define PIN_LED_VERMELHO 8
-
+#include "leds.hpp"
 
 void setup() {
   sensorUltrassonico::initUltrassonico();
-  pinMode(PIN_LED_VERDE, OUTPUT);
-  pinMode(PIN_LED_AMARELO, OUTPUT);
-  pinMode(PIN_LED_VERMELHO, OUTPUT);
+  leds::initLeds();
 }
 
 void loop() {
 
-
-  if(sensorUltrassonico::distancia < 100 && sensorUltrassonico::distancia >= 50) {
-    digitalWrite(PIN_LED_VERDE, HIGH);
-    digitalWrite(PIN_LED_AMARELO, LOW);
-    digitalWrite(PIN_LED_VERMELHO, LOW);
-  }
-  
-  else if(sensorUltrassonico::distancia < 50 && sensorUltrassonico::distancia >= 5) {
-    digitalWrite(PIN_LED_VERDE, LOW);
-    digitalWrite(PIN_LED_AMARELO, HIGH);
-    digitalWrite(PIN_LED_VERMELHO, LOW);
-  }
-  
-  else if(sensorUltrassonico::distancia < 5) {
-    digitalWrite(PIN_LED_VERDE, LOW);
-    digitalWrite(PIN_LED_AMARELO, LOW);
-    digitalWrite(PIN_LED_VERMELHO, HIGH);
-  }
-
-  else {
-    digitalWrite(PIN_LED_VERDE, LOW);
-    digitalWrite(PIN_LED_AMARELO, LOW);
-    digitalWrite(PIN_LED_VERMELHO, LOW);
-  }
+  leds::verificaLeds(sensorUltrassonico::distancia);
 
 }
