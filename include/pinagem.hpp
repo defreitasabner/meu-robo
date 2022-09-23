533c2dce6417d8bffbb35be27798171f5bfe2ed2
Abner Freitas <defreitasabner@gmail.com>
2022-09-22 21:10:34 -0300

    Adicionado arquivo pinagem.hpp para incluir as pinagens.

---
+++ include/pinagem.hpp
@@ -0,0 +1,10 @@
+namespace pin {
+    // pinagem LEDs
+    #define PIN_LED_VERDE 4
+    #define PIN_LED_AMARELO 7 
+    #define PIN_LED_VERMELHO 8
+
+    // pinagem Sensor Ultrassônico
+    #define PIN_SENSOR_ULTRASSONICO_ECHO 2
+    #define PIN_SENSOR_ULTRASSONICO_TRIGGER 12
+}
