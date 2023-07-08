11113e2de43cfbbb894a71a1dec981ea99171c47
Abner Freitas <defreitasabner@gmail.com>
2023-07-07 21:33:56 -0300

    Alterada main com novos módulos funcionando.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
 
 #include "ultrasonicSensor.hpp"
-#include "leds.hpp"
 #include "locomotion.hpp"
 
 void setup() {
   Serial.begin(9600);
   ultrasonicSensor::init();
-  locomotion::motors.init();
-  leds::init();
+  locomotion::init();
 }
 
 void loop() {
-  locomotion::motors.forward(100);
-  leds::verifyDistance(ultrasonicSensor::distance);
-  //Serial.println(String(ultrasonicSensor::distance));
+  if(ultrasonicSensor::distance > 10)
+  {
+    locomotion::forward();
+    Serial.println("Indo para frente");
+  }
+  else
+  {
+    locomotion::backward();
+    Serial.println("Indo para trás");
+  }
 }
